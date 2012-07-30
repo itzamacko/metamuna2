e38b86417b313c8ed8304cae48753d47ed36f547
Itzamacko <anthony.marchante@me.com>
2012-07-29 22:41:12 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metamuna2::Application.config.secret_token = '4ba5f255f347e2f6091b2f9207d43960933fba3d21428f45aa9ce28bd65be7599a832b3237de392e50f927ed5d951792c9eec59a64619d18ba2fa0e26424b80a'
