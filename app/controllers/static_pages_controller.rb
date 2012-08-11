166b9303777084966d0a8e635d49c0e772748528
Itzamacko <anthony.marchante@me.com>
2012-08-11 14:43:04 -0400

    fucking around

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    render :layout => false
   end
-
+  
   def help
+  end
+  
+  def about
+    render :layout => false
+
+  end
+  
+  def basic
+    render :layout => false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Metamuna2::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
+ 
+  root to: 'static_pages#home'
+  
+  match '/signup',  to: 'users#new'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
 
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
+  root :to => 'home#index'
   devise_for :users
   resources :users, :only => [:show, :index]
 end
